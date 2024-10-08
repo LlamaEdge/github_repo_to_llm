fd03fec193db4dcb32b2b289b73bd0e0fa0e24dd
staru09 <arusharmazxx000@gmail.com>
2024-10-08 10:55:06 +0530

    final_script added

--- scripts/Helper/local_csv.py
+++ scripts/Helper/local_csv.py
@@ -1,7 +1,13 @@
 import csv
+import os
+import sys
 
-input_csv = r''
-output_csv = r''
+
+csv.field_size_limit(10**9) 
+
+# Input and output file paths
+input_csv = r'C:\Users\91745\OneDrive\Desktop\Github_analyser\output\local_repo\llamaedge.csv'
+output_csv = r'C:\Users\91745\OneDrive\Desktop\Github_analyser\output\local_repo\llamaedge_merge.csv'
 
 
 with open(input_csv, mode='r', newline='', encoding='utf-8') as infile, \
@@ -14,7 +20,24 @@ with open(input_csv, mode='r', newline='', encoding='utf-8') as infile, \
         path = row['Path']
         content = row['Content']
         
-        new_content = f"language: {path}\n{content}"
+
+        extension = os.path.splitext(path)[1]
+        
+        if extension == '.md':
+            # Format for markdown files
+            new_content = f"The following is a markdown document located at {path}\n------\n{content}\n------"
+        elif extension == '.rs':
+            # Format for Rust code files
+            new_content = f"```rust:{path}\n{content}\n```"
+        elif extension == '.sh':
+            # Format for shell script files
+            new_content = f"```bash:{path}\n{content}\n```"
+        elif extension == '.py':
+            # Format for Python files
+            new_content = f"```python:{path}\n{content}\n```"
+        else:
+            new_content = f"language: {path}\n{content}"
+        
         writer.writerow([new_content])
 
 print(f"Transformed CSV has been written to {output_csv}")
